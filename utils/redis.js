41e74205c28e6899d1d0bf9595d2b5d63333a399
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-13 00:13:01 +0100

    client

---
+++ utils/redis.js
@@ -0,0 +1,31 @@
+import redis from 'redis'
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (err) => {
+      console.error('Redis client not connected to the server:', err);
+    });
+  }
+
+  async isAlive() {
+    try {
+      await this.client.ping();
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async get(key) {
+    return await this.client.get(key);
+  }
+
+  async set(key, value, durationInSeconds) {
+    await this.client.set(key, value, 'EX', durationInSeconds);
+  }
+
+  async del(key) {
+    await this.client.del(key);
+  }
+}
