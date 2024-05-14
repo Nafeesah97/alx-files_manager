fd43f9c386ed19514dac2089ee503ccdef01cc97
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-14 18:40:48 +0100

    route 3

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-import router from './routes';
+import route from './routes';
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-router(app);
+route(app);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
