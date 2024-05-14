b2baeccb1dda6ce676546cd76462c3caa9d24dc6
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-14 18:25:31 +0100

    route 3

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const AppController = require('../controllers/AppController');
 
-const router = express.Router();
+const router = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
+}
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const routes = require('./routes')
+import router from './routes';
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+router(app);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
