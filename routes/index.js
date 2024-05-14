a6b9c6d96a5c0ca70630b9422a712cc2af74644d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-14 18:36:12 +0100

    route 6

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-const dbClient = require('../utils/db');
-const redisClient = require('../utils/redis');
 const express = require('express');
 const AppController = require('../controllers/AppController');
 
-const router = express.Router();
-
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
+const router = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
+}
 
 module.exports = router;
