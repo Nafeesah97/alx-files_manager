2325913faa405b924c63c00630ba2809e58bae98
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-14 14:11:21 +0100

    database 2

--- utils/db.js
+++ utils/db.js
@@ -7,7 +7,7 @@ class DBClient {
     const db = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/${db}`;
 
-    this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
+    this.client = new MongoClient(dbURL, { useUnifiedTopology: true });
     this.client.connect();
   }
 
