8e1d16a8b9bc066e5d6118ea5c19fab49226bd9d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-14 14:44:32 +0100

    database 4

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-import mongodb from 'mongodb';
+import { MongoClient } from 'mongodb'; // Import MongoClient from 'mongodb'
 
 class DBClient {
   constructor() {
@@ -7,22 +7,22 @@ class DBClient {
     const db = process.env.DB_DATABASE || 'files_manager';
     const url = `mongodb://${host}:${port}/${db}`;
 
-    this.client = new mongodb.MongoClient(url, { useUnifiedTopology: true });
-    this.client.connect();
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect(); // Connect to MongoDB server
   }
 
   async isAlive() {
-    return this.client.isConnected();
+    return this.client.isConnected(); // Check if client is connected
   }
 
   async nbUsers() {
     const collection = this.client.db().collection('users');
-    return collection.countDocuments();
+    return collection.countDocuments(); // Get count of documents in 'users' collection
   }
 
   async nbFiles() {
     const collection = this.client.db().collection('files');
-    return collection.countDocuments();
+    return collection.countDocuments(); // Get count of documents in 'files' collection
   }
 }
 
